6e584ce8ebc17d976d70d83dbdba6485eeb3eb91
Roger Lindsjö <roger@tilialacus.net>
2021-04-05 17:51:12 +0200

    Introduce size constant

--- src/main/java/net/tilialacus/lightsout/Board.java
+++ src/main/java/net/tilialacus/lightsout/Board.java
@@ -3,24 +3,26 @@ package net.tilialacus.lightsout;
 public class Board {
 
     public static final long LIGHTS_OUT = 0b00000_00000_00000_00000_00000L;
+    public static final int SIZE = 5;
+    public static final long MASK = (1 << SIZE * SIZE) - 1;
 
     public static boolean completed(long state) {
-        return (state & 0x1FFFFFF) == 0;
+        return (state & MASK) == 0;
     }
 
     public static long toggle(long state, int x, int y) {
-        state = state ^ (1 << (x + y * 5));
+        state = state ^ (1 << (x + y * SIZE));
         if (y > 0) {
-            state = state ^ (1 << (x + (y - 1) * 5));
+            state = state ^ (1 << (x + (y - 1) * SIZE));
         }
         if (y < 4) {
-            state = state ^ (1 << (x + (y + 1) * 5));
+            state = state ^ (1 << (x + (y + 1) * SIZE));
         }
         if (x > 0) {
-            state = state ^ (1 << ((x - 1) + y * 5));
+            state = state ^ (1 << ((x - 1) + y * SIZE));
         }
         if (x < 4) {
-            state = state ^ (1 << ((x + 1) + y * 5));
+            state = state ^ (1 << ((x + 1) + y * SIZE));
         }
         return state;
     }
