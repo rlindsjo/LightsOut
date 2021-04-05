3a370c867e6092641eeac57f9315aa2138dd5393
Roger Lindsjö <roger@tilialacus.net>
2021-04-05 11:36:56 +0200

    Toggle functionality

--- src/main/java/net/tilialacus/lightsout/Board.java
+++ src/main/java/net/tilialacus/lightsout/Board.java
@@ -2,7 +2,26 @@ package net.tilialacus.lightsout;
 
 public class Board {
 
+    public static final long LIGHTS_OUT = 0b00000_00000_00000_00000_00000L;
+
     public static boolean completed(long state) {
         return (state & 0x1FFFFFF) == 0;
+    }
+
+    public static long toggle(long state, int x, int y) {
+        state = state ^ (1 << (x + y * 5));
+        if (y > 0) {
+            state = state ^ (1 << (x + (y - 1) * 5));
+        }
+        if (y < 4) {
+            state = state ^ (1 << (x + (y + 1) * 5));
+        }
+        if (x > 0) {
+            state = state ^ (1 << ((x - 1) + y * 5));
+        }
+        if (x < 4) {
+            state = state ^ (1 << ((x + 1) + y * 5));
+        }
+        return state;
     }
 }
--- src/test/java/net/tilialacus/lightsout/BoardTest.java
+++ src/test/java/net/tilialacus/lightsout/BoardTest.java
@@ -2,9 +2,14 @@ package net.tilialacus.lightsout;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static net.tilialacus.lightsout.Board.LIGHTS_OUT;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BoardTest {
+
     @Test
     void lightsOut() {
         assertAll(
@@ -19,6 +24,25 @@ class BoardTest {
                 () -> assertFalse(Board.completed(1)),
                 () -> assertFalse(Board.completed(100)),
                 () -> assertFalse(Board.completed(298756))
+        );
+    }
+
+    @Test
+    void toggle() {
+        assertAll(
+                () -> assertEquals(0b00000_00100_01110_00100_00000L, Board.toggle(LIGHTS_OUT, 2, 2)),
+
+                () -> assertEquals(0b00000_00000_00000_00001_00011L, Board.toggle(LIGHTS_OUT, 0, 0)),
+                () -> assertEquals(0b00011_00001_00000_00000_00000L, Board.toggle(LIGHTS_OUT, 0, 4)),
+                () -> assertEquals(0b11000_10000_00000_00000_00000L, Board.toggle(LIGHTS_OUT, 4, 4)),
+                () -> assertEquals(0b00000_00000_00000_10000_11000L, Board.toggle(LIGHTS_OUT, 4, 0)),
+
+                () -> assertEquals(0b00000_00000_00000_00100_01110L, Board.toggle(LIGHTS_OUT, 2, 0)),
+                () -> assertEquals(0b00000_00001_00011_00001_00000L, Board.toggle(LIGHTS_OUT, 0, 2)),
+                () -> assertEquals(0b00000_10000_11000_10000_00000L, Board.toggle(LIGHTS_OUT, 4, 2)),
+                () -> assertEquals(0b01110_00100_00000_00000_00000L, Board.toggle(LIGHTS_OUT, 2, 4)),
+
+                () -> assertEquals(LIGHTS_OUT, Board.toggle(0b00000_00100_01110_00100_00000L, 2, 2))
         );
     }
 }
